f2ef8555a4e0b297f001590378df7f06f565fbcc
Sebastian Nocke <sjno@gmx.de>
2024-02-01 23:26:13 +0100

    Added component to the editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,8 @@
 use eframe::{egui::{self, CentralPanel, Sense}, epaint::{vec2, Pos2, Rect, Vec2}};
 use egui::*;
+use simulator::function::Function;
 
-use self::elements::{EditorInput, EditorOutput, EditorComponent, EditorLine};
+use self::elements::{EditorComponent, EditorInput, EditorLine, EditorOutput, EditorShape};
 
 mod elements;
 
@@ -12,10 +13,15 @@ pub struct Editor {
 impl Editor {
     pub fn update(&mut self, ctx: &egui::Context) {
         CentralPanel::default().show(ctx, |ui| {
-
-            if ui.button("Input").clicked() {
-                self.circuit.inputs.push(EditorInput::new(pos2(20.0, 20.0)));
-            }
+            
+            let button_responses = ui.horizontal_top(|ui| {
+                let input_response = ui.button("Input");
+                let component_response = ui.button("Component");
+                
+                (input_response, component_response)
+            });
+            
+            let (input_response, component_response) = button_responses.inner;
 
             let (response, painter) =
                 ui.allocate_painter(vec2(ui.available_width(), ui.available_height()), Sense::hover());
@@ -24,6 +30,14 @@ impl Editor {
                 Rect::from_min_size(Pos2::ZERO, response.rect.size()),
                 response.rect
             );
+            
+            if input_response.clicked() {
+                self.circuit.inputs.push(EditorInput::new(response.rect.center()))
+            }
+
+            if component_response.clicked() {
+                self.circuit.components.push(EditorComponent::new(response.rect.center(), Function::And));
+            }
             
             let connected_lines_start = self.circuit.input_connected_lines_start();
             let connected_lines_end = self.circuit.input_connected_lines_end();
@@ -52,13 +66,30 @@ impl Editor {
                         self.circuit.lines.push(EditorLine::from_single_pos(input.position + vec2(20.0, 0.0)));
                     }
 
-                    input.shape(to_screen, point_response.dragged())
+                    input.get_shape(to_screen, point_response.dragged())
                 })
                 .collect();
-
-            self.circuit.input_reconnect_lines_start(connected_lines_start);
-            self.circuit.input_reconnect_lines_end(connected_lines_end);
             
+            let component_shapes: Vec<Shape> = self.circuit
+                .components
+                .iter_mut()
+                .enumerate()
+                .map(|(i, component)| {
+                    let size = Vec2::splat(60.0);
+
+                    let point_in_screen = to_screen.transform_pos(component.position);
+                    let point_in_rect = Rect::from_center_size(point_in_screen, size);
+                    let point_id = response.id.with("component".to_owned() + &i.to_string());
+                    let point_response = ui.interact(point_in_rect, point_id, Sense::drag());
+                    
+                    component.position += point_response.drag_delta();
+                    component.position = to_screen.from().clamp(component.position);
+                    
+                    component.get_shape(to_screen, point_response.dragged())
+                })
+                .collect();
+            
+            let mut dragged_line = None;
             let line_shapes: Vec<Shape> = self.circuit
                 .lines
                 .iter_mut()
@@ -71,22 +102,35 @@ impl Editor {
                     let start_id = response.id.with("line start".to_string() + &i.to_string());
                     let start_response = ui.interact(start_rect, start_id, Sense::drag());
                     
-                    line.start += start_response.drag_delta();
-                    line.start = to_screen.from().clamp(line.start);                    
-
+                    if start_response.dragged() {
+                        line.start += start_response.drag_delta();
+                        line.start = to_screen.from().clamp(line.start);
+                        
+                        dragged_line = Some(i);
+                    }
+                    
                     let end_in_screen = to_screen.transform_pos(line.end);
                     let end_rect = Rect::from_center_size(end_in_screen, size);
                     let end_id = response.id.with("line end".to_string() + &i.to_string());
                     let end_response = ui.interact(end_rect, end_id, Sense::drag());
                     
-                    line.end += end_response.drag_delta();
-                    line.end = to_screen.from().clamp(line.end);                    
+                    if end_response.dragged() {
+                        line.end += end_response.drag_delta();
+                        line.end = to_screen.from().clamp(line.end);
+
+                        dragged_line = Some(i);                        
+                    }
                     
-                    line.shape(to_screen, start_response.dragged(), end_response.dragged())
+                    line.get_shape(to_screen, (start_response.dragged(), end_response.dragged()))
                 })
                 .collect();
+                
+            self.circuit.input_reconnect_lines_start(connected_lines_start, dragged_line);
+            self.circuit.input_reconnect_lines_end(connected_lines_end, dragged_line);
 
-            input_shapes.iter().chain(line_shapes.iter())
+            input_shapes.iter()
+                .chain(component_shapes.iter())
+                .chain(line_shapes.iter())
                 .for_each(|shape| {
                     painter.add(shape.clone());
                 });
@@ -153,13 +197,19 @@ impl EditorCircuit {
         .collect()
     }
     
-    fn input_reconnect_lines_start(&mut self, connection_list: Vec<Vec<usize>>) {
+    fn input_reconnect_lines_start(&mut self, connection_list: Vec<Vec<usize>>, dragged_line: Option<usize>) {
         connection_list.iter()
             .enumerate()
             .for_each(|(input_index, line_indices)| {
                 let connector_position = self.inputs[input_index].position + Vec2::new(20.0, 0.0);
 
                 line_indices.iter()
+                    .filter(|&line_index| {
+                        match dragged_line {
+                            Some(dragged_index) => *line_index != dragged_index,
+                            None => true,
+                        }
+                    })
                     .for_each(|&line_index| {
                         self.lines[line_index].start = connector_position;
                     })
@@ -167,13 +217,19 @@ impl EditorCircuit {
             })
     }
 
-    fn input_reconnect_lines_end(&mut self, connection_list: Vec<Vec<usize>>) {
+    fn input_reconnect_lines_end(&mut self, connection_list: Vec<Vec<usize>>, dragged_line: Option<usize>) {
         connection_list.iter()
             .enumerate()
             .for_each(|(input_index, line_indices)| {
                 let connector_position = self.inputs[input_index].position + Vec2::new(20.0, 0.0);
 
                 line_indices.iter()
+                    .filter(|&line_index| {
+                        match dragged_line {
+                            Some(dragged_index) => *line_index != dragged_index,
+                            None => true,
+                        }
+                    })
                     .for_each(|&line_index| {
                         self.lines[line_index].end = connector_position;
                     })
--- src/editor/elements.rs
+++ src/editor/elements.rs
@@ -1,24 +1,13 @@
-use eframe::{epaint::{vec2, Color32, FontId, Pos2, Shape, Stroke, Vec2}, egui::{Painter, Rect}, emath::{Align2, RectTransform}};
+use eframe::{epaint::{vec2, Color32, Pos2, Shape, Stroke, Vec2}, egui::Rect, emath::RectTransform};
 use simulator::function::Function;
 
-const STROKE_WIDTH: f32 = 5.0;
-const STROKE_COLOR: Color32 = Color32::WHITE;
+pub trait EditorShape {
+    type DraggedInfo;
 
-#[derive(Clone, Copy, Debug)]
-pub struct Position {
-    x: i32,
-    y: i32,
+    fn get_shape(&self, transform: RectTransform, dragged_info: Self::DraggedInfo) -> Shape;
 }
 
-impl Position {
-    pub fn new(x: i32, y: i32) -> Self {
-        Self {
-            x,
-            y,
-        }
-    }
-}
-
+#[derive(Debug)]
 pub struct EditorInput {
     pub position: Pos2,
 }
@@ -29,11 +18,15 @@ impl EditorInput {
             position,
         }
     }
+}
 
-    pub fn shape(&self, transform: RectTransform, dragged: bool) -> Shape {
+impl EditorShape for EditorInput {
+    type DraggedInfo = bool;
+
+    fn get_shape(&self, transform: RectTransform, dragged_info: Self::DraggedInfo) -> Shape {
         let transformed_position = transform.transform_pos(self.position);
         
-        let border_stroke = if dragged {
+        let border_stroke = if dragged_info {
             Stroke::new(4.0, Color32::WHITE)
         } else {
             Stroke::new(3.0, Color32::WHITE)
@@ -47,47 +40,27 @@ impl EditorInput {
     }
 }
 
+#[derive(Debug)]
 pub struct EditorOutput {
-    position: Position,
+    position: Pos2,
 }
 
 impl EditorOutput {
-    pub fn new(position: Position) -> Self {
+    pub fn new(position: Pos2) -> Self {
         Self {
             position,
         }
     }
 }
 
-impl RealPosition for EditorOutput {
-    fn set_position(&mut self, position: Position) {
-        self.position = position;
-    }
-
-    fn get_position(&self) -> Position {
-        self.position
-    }
-}
-
-impl Draw for EditorOutput {
-    fn draw(&self, painter: &Painter, scaling: f32, area: Rect) {
-        let shape_stroke = Stroke::new(STROKE_WIDTH, STROKE_COLOR);
-        let real_position = Self::get_real_position(&self, scaling, area);
-
-        painter.circle_stroke(real_position, scaling, shape_stroke);
-        painter.circle_stroke(real_position, (scaling * 2.0) / 3.0, shape_stroke);
-        painter.circle_filled(real_position - Vec2::new(scaling, 0.0), STROKE_WIDTH, Color32::RED);
-    }
-}
-
-
+#[derive(Debug)]
 pub struct EditorComponent {
-    position: Position,
-    function: Function,
+    pub position: Pos2,
+    pub function: Function,
 }
 
 impl EditorComponent {
-    pub fn new(position: Position, function: Function) -> Self {
+    pub fn new(position: Pos2, function: Function) -> Self {
         Self {
             position,
             function,
@@ -95,23 +68,24 @@ impl EditorComponent {
     }
 }
 
-impl RealPosition for EditorComponent {
-    fn set_position(&mut self, position: Position) {
-        self.position = position;
-    }
+impl EditorShape for EditorComponent {
+    type DraggedInfo = bool;
 
-    fn get_position(&self) -> Position {
-        self.position
-    }
-}
+    fn get_shape(&self, transform: RectTransform, dragged_info: Self::DraggedInfo) -> Shape {
+        let transformed_position = transform.transform_pos(self.position);
+        
+        let border_stroke = if dragged_info {
+            Stroke::new(4.0, Color32::WHITE)
+        } else {
+            Stroke::new(3.0, Color32::WHITE)
+        };
 
-impl Draw for EditorComponent {
-    fn draw(&self, painter: &Painter, scaling: f32, area: Rect) {
-        let shape_stroke = Stroke::new(STROKE_WIDTH, STROKE_COLOR);
-        let real_position = Self::get_real_position(&self, scaling, area);
+        let border = Shape::rect_stroke(Rect::from_center_size(transformed_position, Vec2::splat(60.0)), 0.0, border_stroke);
+        let in_connector0 = Shape::circle_filled(transformed_position + vec2(-30.0, -15.0), 5.0, Color32::RED);
+        let in_connector1 = Shape::circle_filled(transformed_position + vec2(-30.0, 15.0), 5.0, Color32::RED);
+        let out_connector0 = Shape::circle_filled(transformed_position + vec2(30.0, 0.0), 5.0, Color32::RED);
 
-        painter.rect_stroke(Rect::from_center_size(real_position, Vec2::splat(scaling * 2.0)), 0.0, shape_stroke);
-        painter.text(real_position, Align2::CENTER_CENTER, "comp", FontId::monospace(12.0), Color32::WHITE);
+        Shape::Vec(vec![border, in_connector0, in_connector1, out_connector0])
     }
 }
 
@@ -122,21 +96,19 @@ pub struct EditorLine {
 }
 
 impl EditorLine {
-    pub fn new(start: Pos2, end: Pos2) -> Self {
-        Self {
-            start,
-            end,
-        }
-    }
-    
     pub fn from_single_pos(pos: Pos2) -> Self {
         Self {
             start: pos,
             end: pos,
         }
     }
-    
-    pub fn shape(&self, transform: RectTransform, start_dragged: bool, end_dragged: bool) -> Shape {
+}
+
+impl EditorShape for EditorLine {
+    type DraggedInfo = (bool, bool);
+
+    fn get_shape(&self, transform: RectTransform, dragged_info: Self::DraggedInfo) -> Shape {
+        let (start_dragged, end_dragged) = dragged_info;
         let real_start = transform.transform_pos(self.start);
         let real_end = transform.transform_pos(self.end);
 
@@ -156,17 +128,4 @@ impl EditorLine {
             Shape::Vec(vec![start_shape, end_shape, line_shape])
         }
     }
-}
-
-pub trait RealPosition {
-    fn set_position(&mut self, position: Position);
-    fn get_position(&self) -> Position;
-
-    fn get_real_position(&self, scaling: f32, area: Rect) -> Pos2 {
-        Pos2::new(self.get_position().x as f32, self.get_position().y as f32) * scaling + area.left_top().to_vec2()
-    }
-}
-
-pub trait Draw {
-    fn draw(&self, painter: &Painter, scaling: f32, area: Rect);
 }
